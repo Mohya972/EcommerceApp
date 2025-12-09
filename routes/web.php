c2d692caf95ba23758f1b75ee5a8a95354ab6eca
mohya972 <mohya972n@gmail.com>
2025-12-09 16:17:19 -0400

    Création de la branche cart

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,10 @@
 <?php
 
-
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
-//use App\Http\Controllers\CartController;
+use App\Http\Controllers\ProductController;
 
 // Page d'accueil
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -17,7 +15,7 @@ Route::get('/products/{product:slug}', [ProductController::class, 'show'])->name
 
 // Catégories
 Route::get('/categories/{category:slug}', [ProductController::class, 'category'])->name('categories.show');
-/*
+
 // Panier (authentification requise)
 Route::middleware(['auth'])->group(function () {
     Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
@@ -26,12 +24,10 @@ Route::middleware(['auth'])->group(function () {
     Route::delete('/cart/remove/{cartItem}', [CartController::class, 'remove'])->name('cart.remove');
     Route::delete('/cart/clear', [CartController::class, 'clear'])->name('cart.clear');
 });
-*/
+
 
 
 /*
-
-use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
