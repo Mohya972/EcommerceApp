ef7d0d523938bc8786bc1669a82720c420e2db68
mohya972 <mohya972n@gmail.com>
2025-12-11 07:51:29 -0400

    push

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use App\Models\CartItem;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
